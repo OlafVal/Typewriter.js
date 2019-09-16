ed1f1d4220fbc9ba693f68cd79eeee4de63a6558
Olaf Val <info@olafval.de>
2019-09-16 23:03:05 +0200

    Add files via upload

---
+++ p5.typewriter.js
@@ -0,0 +1,47 @@
+// p5.Typewriter
+// by Olaf Val
+// CC BY 4.0
+
+function Typewriter() {
+
+  this.TyWrTextAll = [];
+  this.TyWrTextPrint = [];
+  this.TyWrTextDisplay = '';
+  this.TyWrL = 0;
+  this.TyWrS = ''
+  this.TyWrTimer = 0;
+  this.TyWrSpeed = 200;
+
+  this.twRestart = function() {
+    this.TyWrL = 0;
+    this.TyWrTextPrint = [];
+  }
+
+  this.twSpeed = function(s) {
+    this.TyWrSpeed = s;
+  }
+
+  this.twCompleted = function() {
+    if (this.TyWrL < this.TyWrTextAll.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  this.twTyp = function(x, y, TyWrText) {
+    this.TyWrTextAll = TyWrText;
+    this.TyWrTextDisplay = join(this.TyWrTextPrint, this.TyWrS);
+    text(this.TyWrTextDisplay, x, y);
+
+    if (millis() > this.TyWrTimer + this.TyWrSpeed) {
+      this.TyWrTimer = millis();
+      if (this.TyWrL < TyWrText.length) {
+
+        append(this.TyWrTextPrint, TyWrText[this.TyWrL]);
+        this.TyWrL++;
+      }
+    }
+  }
+
+}
