29d28bf031ac09752a26048700525227ba163d43
Olaf Val <info@olafval.de>
2020-05-04 17:02:25 +0200

    Add files via upload

---
+++ Examples/twBox-example.js
@@ -0,0 +1,26 @@
+// p5.Typewriter
+// by Olaf Val
+
+function setup() {
+  createCanvas(400, 400);
+
+  tw1 = new Typewriter();
+}
+
+function draw() {
+  background(215);
+
+  noStroke();
+
+  tw1.twBox(50, 50, 300, 300);
+  
+  tw1.twAlign('CENTER');
+  
+  tw1.twSpeed(10); // potional
+  tw1.twType('p5.typewriter.js<br>This is my slightly longer sample text to demonstrate the automatic line break. This arises as soon as the twBox command is used. It also overrides the positioning of the text. <br><br> Have fun!', 10, 10);
+
+}
+
+function mousePressed() {
+  tw1.twRestart(); // This also restarts line two
+}
